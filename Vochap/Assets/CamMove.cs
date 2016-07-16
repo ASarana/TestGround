a5fa4f46331054c4717059749385a7181516786c
rolandxl <rolandxxxl@gmail.com>
2016-07-16 21:00:12 +0300

    Камера

--- Vochap/Assets/CamMove.cs
+++ Vochap/Assets/CamMove.cs
@@ -4,17 +4,20 @@ using System.Collections;
 public class CamMove : MonoBehaviour
 {
 
-    private Vector3 camzero;
+    public Vector3 camzero;
+    public Vector3 camtrans;
     public Transform hero;
-
     void Start()
     {
-        camzero = hero.position - this.transform.position; //начальное положение камера относительно пакмана
+        // camzero = new Vector3(-40, -44, 2);
+        //camzero = hero.position - this.transform.position; //начальное положение камера относительно пакмана
+        this.transform.rotation = Quaternion.Euler(camtrans);
     }
 
     // Update is called once per frame
     void Update()
     {
         this.transform.position = hero.position - camzero; //двигаем камеру в след за пакманом
+       // print(camzero);
     }
 }
