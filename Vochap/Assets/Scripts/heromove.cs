d3a5d93ee26b013893df492ec14be08235afbc77
rolandxl <rolandxxxl@gmail.com>
2016-07-20 19:04:04 +0300

    суперпрыжок

--- Vochap/Assets/Scripts/heromove.cs
+++ Vochap/Assets/Scripts/heromove.cs
@@ -14,12 +14,22 @@ public class heromove : MonoBehaviour
     private float v;
     private float h;
     private float Y;
+    Vector3 gravi;
+    bool jump;
+    public float m;
+    float g;
+    public float V0;
     //public Transform zeropos;
 
     void Start()
     {
         hero = GetComponent<CharacterController>();
         cam = Camera.main.transform;
+        jump = false;
+        gravi = -Vector3.up;
+        g = (float)9.8;
+        m = 50;
+        V0 = 1;
     }
 
     // Update is called once per frame
@@ -28,13 +38,22 @@ public class heromove : MonoBehaviour
 
         h = CrossPlatformInputManager.GetAxis("Horizontal");
         v = CrossPlatformInputManager.GetAxis("Vertical");
+        jump = CrossPlatformInputManager.GetButton("Jump");
 
         camForward = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;
         //рассчитаем вектор движения 
         move = (v * camForward + h * cam.right).normalized;
         hero.Move(speed * move); //двигаем 
-        hero.Move(-Vector3.up); // прижимаем к земле
-        if (move != new Vector3(0, 0, 0))
+        hero.Move(gravi); // прижимаем к земле
+        if (move != Vector3.zero)
             transform.forward = move;
+        if(jump)
+        {
+            V0 = V0 - g * Time.deltaTime;
+            gravi.y = V0;
+        }
+
+        if (!jump) V0 = 5;
+
     }
 }
