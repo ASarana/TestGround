a17b4486c685546774a0f4df8876d6215d84ea4f
rolandxl <rolandxxxl@gmail.com>
2016-10-01 11:42:19 +0300

    уборка
    1. навел порядок в скриптах
    2. повыносил в отдельные процедуры движения
    3. вынес дебаг в отдельный скрипт

--- Vochap/Assets/Scripts/heromove.cs
+++ Vochap/Assets/Scripts/heromove.cs
@@ -6,50 +6,43 @@ public class heromove : MonoBehaviour
 {
 
     // Use this for initialization
-    private CharacterController hero; //контроллер чтобы его двигать
-    public Animator anima;
-    public float speed; //скорость и камеры
+    public MyDebug Mydebug; // скрипт для отладки, котрорый выводит в тестовое поле все отлаживаемые переменые
+    private CharacterController hero; //контроллер чтобы двигать персонажа
+    Animator anima;
+    public float RunSpeed; //множитель скорости персонажа
     private Vector3 move; //вектор движения 
     private Transform cam; //это чтобы рассчитать движение относительно камеры
-    private Vector3 camForward;
-    private float v;
-    private float h;
-    private float Y;
-    Vector3 gravi;
-    bool jump;
-    bool injump;
-    bool testkey;
-    public float g;
-    public float V0;
-    float V;
+    private Vector3 camForward; //это чтобы рассчитать движение относительно камеры
+    private float v; //для хранения вертикальной оси ввода
+    private float h; //для хранения горизонтальной оси ввода
+    Vector3 gravi; //движение персонажа под гравитацией
+    bool jump; //хранит нажатие кнопки прыжка
+    bool injump; //хранит состояние персожажа, в прыжке-ли он
+    bool testkey; 
+    public float g; //ускорение свободного падения
+    public float JumpSpeed;  //сила толчка при прыжке
+    float V; //вертикальная скорость персонажа припрыжке (может быть отрицательная когда летит вниз)
     //public Transform zeropos;
-    public TextMesh debtext;
-    string hitname;
-    public float pushPower = 2.0F;
-    bool horpush;
-    bool vertpush;
-    Vector3 lefts;
-    Vector3 rightss;
-    Vector3 forwards; 
-    Vector3 backs;
-    Vector3 hitpoint;
-    static GameObject rightgun;
-    static GameObject backgun;
-    GameObject leftgun;
+    public TextMesh debtext; //текстовое поле для дебага
+    public float pushPower; //множитель скорости толкания предмета
+    bool horpush; //если персонаж уже толкает по горизонтали
+    bool vertpush; //если персонаж уже толкает по вертикали
+    static GameObject rightgun; //контейнр под оружие в правой руке
+    static GameObject backgun; //контейнер под оружие за спиной
+    static GameObject backgun2; //контейнер под второе оружие за спиной
+    static GameObject leftgun; //контейнер под оружие в левой руке
 
     void Start()
     {
-        hero = GetComponent<CharacterController>();
-        anima = GetComponent<Animator>();
+        hero = GetComponent<CharacterController>(); //инициализация компонента
+        anima = GetComponent<Animator>(); //инициализация компонента
         cam = Camera.main.transform;
         jump = false;
         injump = false;
         testkey = false;
         gravi = -Vector3.up;
-        // g = 5f;
          horpush = false;
          vertpush=false;
-        // shotgun_vochap, machete_vochap, hand.R, neck
 
         rightgun = GameObject.Find("machete_vochap1");
         backgun = GameObject.Find("machete_vochap");
@@ -63,106 +56,29 @@ public class heromove : MonoBehaviour
         if (!horpush) v = CrossPlatformInputManager.GetAxis("Vertical");
         jump = CrossPlatformInputManager.GetButton("Jump");
         testkey = CrossPlatformInputManager.GetButton("Fire1");
-        if (testkey)
-        {
-            anima.SetBool("testanim", true);
-           //  GameObject.Find("machete_vochap").SetActive(false);
-           //  GameObject.Find("machete_vochap1").SetActive(true);
-          //  Object.Instantiate(rightgun);
-            rightgun.SetActive(true);
-            backgun.SetActive(false);
-        }
-        else
-        {
-            //anima.SetBool("testanim", false);
-            //  GameObject.Find("machete_vochap").SetActive(true);
-            // GameObject.Find("machete_vochap1").SetActive(false);
-            rightgun.SetActive(false);
-            backgun.SetActive(true);
-        }
+       
 
-        //anima.SetBool("idletowalk", false);
         camForward = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;
         //рассчитаем вектор движения 
         move = (v * camForward + h * cam.right).normalized;
+
+        WeaponChange(testkey);
+        MyHeroMove(move, jump, gravi, anima);
+
         debtext.transform.position = this.transform.position + new Vector3(-10, -11, -20);
 
-        debtext.text = "hero.isGrounded:" + hero.isGrounded.ToString() + '\n'
-            + "injump:" + injump.ToString() + '\n'
-            + "jump:" + jump.ToString() + '\n'
-            + "V:" + V.ToString() + '\n'
-            + "gravi" + gravi.ToString() + '\n'
-            + "anima idletowalk:" + anima.GetBool("idletowalk") + '\n'
-            + "anima jumpup:" + anima.GetBool("jumpup") + '\n'
-            + "anima jumpdown:" + anima.GetBool("jumpdown") + '\n'
-            + "anima fall:" + anima.GetBool("fall") + '\n'
-            + "anima push:" + anima.GetBool("push") + '\n'
-            + "hit object:" + hitname + '\n'
-            + "hit point left:" + lefts.ToString() + '\n'
-            + "hit point right:" + rightss.ToString() + '\n'
-            + "hit point forw:" + forwards.ToString() + '\n'
-            + "hit point back:" + backs.ToString() + '\n'
-            + "hit point:" + hitpoint.ToString() + '\n';
-        if (move != Vector3.zero)
-        {
-            transform.forward = move;
-            if(!injump && hero.isGrounded)
-            anima.SetBool("idletowalk", true);
-            hero.Move(speed * move);
-        }
-        if (move == Vector3.zero)
-        {
-            anima.SetBool("idletowalk", false);
-             anima.SetBool("push", false);
-            horpush = false;
-            vertpush = false;
-        }
-            
-        if (!injump && !hero.isGrounded)
-        {
-            // hero.Move(-Vector3.up);
-            // anima.SetBool("jumpdown", false);
-          //  V = 0;
-            V = V - g * Time.fixedDeltaTime;
-            gravi.y = V;
-            hero.Move(gravi);
-         //   anima.SetBool("idletowalk", false);
-            anima.SetBool("fall", true);
-        }
-        if (jump && hero.isGrounded && !injump && !anima.GetBool("push"))
-          {
-            injump = true;
-            V = V0;
-            V = V - g * Time.fixedDeltaTime;
-            gravi.y = V;
-            hero.Move(gravi);
-            anima.SetBool("jumpup", true);
-          //  anima.SetBool("idletowalk", false);
-        }
-          if (injump && !hero.isGrounded)
-          {
-            V = V - g * Time.fixedDeltaTime;
-            gravi.y = V;
-            hero.Move(gravi);
-            if (V <= 0)
-            {
-                anima.SetBool("jumpup", false);
-                anima.SetBool("fall", true);
-            }
-        }
-        if (!injump && hero.isGrounded)
-        {
-            anima.SetBool("jumpdown", false);
-            anima.SetBool("fall", false);
-            V = 0;
-        }
-        if (injump && hero.isGrounded)
-          {
-            injump = false;
-            V = 0;
-            anima.SetBool("fall", false);
-            anima.SetBool("jumpdown", true);
-        }
+        Mydebug.AddParamDebug("hero.isGrounded", hero.isGrounded.ToString());
+        Mydebug.AddParamDebug("injump", injump.ToString());
+        Mydebug.AddParamDebug("jump", jump.ToString());
+        Mydebug.AddParamDebug("V", V.ToString());
+        Mydebug.AddParamDebug("gravi", gravi.ToString());
+        Mydebug.AddParamDebug("idletowalk", anima.GetBool("idletowalk").ToString());
+        Mydebug.AddParamDebug("jumpup", anima.GetBool("jumpup").ToString());
+        Mydebug.AddParamDebug("jumpdown", anima.GetBool("jumpdown").ToString());
+        Mydebug.AddParamDebug("fall", anima.GetBool("fall").ToString());
+        Mydebug.AddParamDebug("push", anima.GetBool("push").ToString());
+        Mydebug.ShowDebug();
+
     }
 
     void OnControllerColliderHit(ControllerColliderHit hit)
@@ -171,17 +87,19 @@ public class heromove : MonoBehaviour
         if (body == null || body.isKinematic || body.gameObject.name=="Terrain")
              return; 
 
-        else hitname = hit.gameObject.name;
-
         if (hit.moveDirection.y < -0.3F)
             return;
 
         if (hero.isGrounded)
         {
             Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
-           
+            Vector3 lefts;
+            Vector3 rightss;
+            Vector3 forwards;
+            Vector3 backs;
+            Vector3 hitpoint;
 
-             forwards = new Vector3(hit.collider.transform.position.x - hit.collider.transform.lossyScale.x/2, hit.collider.transform.position.y, hit.collider.transform.position.z);
+            forwards = new Vector3(hit.collider.transform.position.x - hit.collider.transform.lossyScale.x/2, hit.collider.transform.position.y, hit.collider.transform.position.z);
              backs = new Vector3(hit.collider.transform.position.x + hit.collider.transform.lossyScale.x / 2, hit.collider.transform.position.y, hit.collider.transform.position.z);
              rightss = new Vector3(hit.collider.transform.position.x, hit.collider.transform.position.y, hit.collider.transform.position.z + hit.collider.transform.lossyScale.z / 2);
              lefts = new Vector3(hit.collider.transform.position.x, hit.collider.transform.position.y, hit.collider.transform.position.z - hit.collider.transform.lossyScale.z / 2);
@@ -197,6 +115,80 @@ public class heromove : MonoBehaviour
                 body.velocity = pushDir * pushPower;
             }
 
+        }
+    }
+
+    void MyHeroMove(Vector3 _move, bool _jump, Vector3 _gravi, Animator _anima)
+    {
+        if (_move != Vector3.zero)
+        {
+            transform.forward = _move;
+            if (!injump && hero.isGrounded)
+                _anima.SetBool("idletowalk", true);
+            hero.Move(RunSpeed * move);
+        }
+        if (_move == Vector3.zero)
+        {
+            _anima.SetBool("idletowalk", false);
+            _anima.SetBool("push", false);
+            horpush = false;
+            vertpush = false;
+        }
+
+        if (!injump && !hero.isGrounded)
+        {
+            V = V - g * Time.fixedDeltaTime;
+            _gravi.y = V;
+            hero.Move(_gravi);
+            _anima.SetBool("fall", true);
+        }
+        if (_jump && hero.isGrounded && !injump && !anima.GetBool("push"))
+        {
+            injump = true;
+            V = JumpSpeed;
+            V = V - g * Time.fixedDeltaTime;
+            _gravi.y = V;
+            hero.Move(_gravi);
+            _anima.SetBool("jumpup", true);
+        }
+        if (injump && !hero.isGrounded)
+        {
+            V = V - g * Time.fixedDeltaTime;
+            _gravi.y = V;
+            hero.Move(_gravi);
+            if (V <= 0)
+            {
+                _anima.SetBool("jumpup", false);
+                _anima.SetBool("fall", true);
+            }
+        }
+        if (!injump && hero.isGrounded)
+        {
+            _anima.SetBool("jumpdown", false);
+            _anima.SetBool("fall", false);
+            V = 0;
+        }
+        if (injump && hero.isGrounded)
+        {
+            injump = false;
+            V = 0;
+            _anima.SetBool("fall", false);
+            _anima.SetBool("jumpdown", true);
+        }
+    }
+
+    void WeaponChange(bool change)
+    {
+        if (change)
+        {
+            anima.SetBool("testanim", true);
+            rightgun.SetActive(true);
+            backgun.SetActive(false);
+        }
+        else
+        {
+            rightgun.SetActive(false);
+            backgun.SetActive(true);
         }
     }
 
